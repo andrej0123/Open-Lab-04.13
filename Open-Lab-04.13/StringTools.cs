5967f53aa110872749ac3af8aa86e3c8993a305a
Andrej Svrček <nepoviem251@gmail.com>
2021-12-17 08:50:00 +0100

    Finds the longest common substring of two strings

--- Open-Lab-04.13/StringTools.cs
+++ Open-Lab-04.13/StringTools.cs
@@ -6,7 +6,23 @@ namespace Open_Lab_04._13
     {
         public string GetLongestCommonSequence(string str1, string str2)
         {
-            throw new NotImplementedException();
+            int[,] a = new int[str1.Length + 1, str2.Length + 1];
+            int row = 0; 
+            int col = 0;   
+
+            for (var i = 0; i < str1.Length; i++)
+                for (var j = 0; j < str2.Length; j++)
+                    if (str1[i] == str2[j])
+                    {
+                        int len = a[i + 1, j + 1] = a[i, j] + 1;
+                        if (len > a[row, col])
+                        {
+                            row = i + 1;
+                            col = j + 1;
+                        }
+                    }
+
+            return str1.Substring(row - a[row, col], a[row, col]);
         }
     }
 }
